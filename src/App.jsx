0159b2805cc882dcba6ac3f373feb5355233a6cd
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2024-12-04 01:47:22 +0530

    commit-master

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>CHANGE 02</h1>
+        <h1>CHANGE 04</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
